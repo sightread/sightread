7870fd5e2d93129d3a2b21af414a6e24b8288f2e
Jake Fried <4656974+samouri@users.noreply.github.com>
2021-10-04 17:18:39 -0400

    toggle isn't configurable but looks ok

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,5 +1,48 @@
-import { useRef, useState } from '@storybook/addons'
-import { useCallback } from 'react'
+import clsx from 'clsx'
+import { useCallback, useRef, useState } from 'react'
+import { css } from '@sightread/flake'
+import { peek } from 'src/utils'
+
+const classes = css({
+  switch: {
+    position: 'relative',
+    display: 'inline-block',
+
+    // Hide default HTML checkbox
+    '& input': {
+      opacity: 0,
+      width: 0,
+      height: 0,
+    },
+  },
+  slider: {
+    position: 'absolute',
+    cursor: 'pointer',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#ccc',
+    transition: '.4s',
+    borderRadius: 20,
+    '&.checked': {
+      backgroundColor: '#2196F3',
+    },
+  },
+  dot: {
+    position: 'absolute',
+    height: 15,
+    width: 15,
+    left: 4,
+    bottom: 4,
+    borderRadius: '50%',
+    backgroundColor: 'white',
+    transition: '.4s',
+    '&.checked': {
+      left: '60%',
+    },
+  },
+})
 
 type ToggleProps = {
   checked: boolean
@@ -8,39 +51,23 @@ type ToggleProps = {
   height?: number
 }
 export default function Toggle(props: ToggleProps) {
-  const [checked, setChecked] = useState(props.checked)
+  const [checked, setChecked] = useState(!!props.checked)
   const inputRef = useRef<HTMLInputElement | null>(null)
   const width = props.width ?? 50
   const height = props.height ?? 24
 
-  const handleClick = useCallback(
-    (event: Event) => {
-      if (!inputRef.current) {
-        return
-      }
-      setChecked(!checked)
-    },
-    [props.checked],
-  )
+  const handleClick = useCallback(() => {
+    if (!inputRef.current) {
+      return
+    }
+    setChecked((prev) => !prev)
+  }, [])
 
   return (
-    <div className={classes} onClick={handleClick} ref={inputRef}>
-      <div className="react-toggle-track">
-        <div className="react-toggle-track-check">{this.getIcon('checked')}</div>
-        <div className="react-toggle-track-x">{this.getIcon('unchecked')}</div>
-      </div>
-      <div className="react-toggle-thumb" />
-
-      <input
-        {...inputProps}
-        ref={(ref) => {
-          this.input = ref
-        }}
-        onFocus={this.handleFocus}
-        onBlur={this.handleBlur}
-        className="react-toggle-screenreader-only"
-        type="checkbox"
-      />
-    </div>
+    <label className={classes.switch} style={{ width, height }}>
+      <input type="checkbox" ref={inputRef} onClick={handleClick} />
+      <span className={clsx(classes.slider, { checked })}></span>
+      <span className={clsx(classes.dot, { checked })}></span>
+    </label>
   )
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as AppBar } from './AppBar'
 export { default as Modal } from './Modal'
 export { default as Select } from './Select'
+export { default as Toggle } from './Toggle'
