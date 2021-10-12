a92faff878dc2c340c0dfb9c589f91387dea073f
Jake Fried <4656974+samouri@users.noreply.github.com>
2021-10-11 21:45:53 -0400

    forgot toggle story

---
+++ src/components/Toggle.stories.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Toggle } from 'src/components'
+
+export default {
+  title: 'Toggle',
+  component: Toggle,
+} as ComponentMeta<typeof Toggle>
+
+const Template: ComponentStory<typeof Toggle> = (args: any) => <Toggle {...args} />
+
+export const ToggleUncontrolled = Template.bind({})
+ToggleUncontrolled.args = {}
+
+export const ToggleControlled = () => {
+  const [checked, setChecked] = useState(false)
+  return <Toggle checked={checked} onChange={setChecked} />
+}
