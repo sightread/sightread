9f3d3d97284233cf1c8f557f101ca5c0d32f92d5
Jake Fried <4656974+samouri@users.noreply.github.com>
2021-11-05 00:14:50 -0400

    fix storybook with stupid plugin because sb doesnt understand non-relative imports.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = ({ config }) => {
   config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules']
+  config.resolve.plugins = [new TsconfigPathsPlugin()]
   return config
 }
