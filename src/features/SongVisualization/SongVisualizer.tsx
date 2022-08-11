a803457a773f16f32fd4901d09d6bc4e5efb82f7
Pigamo <chosongames@gmail.com>
2022-08-11 14:42:54 -0400

    Testing no events

--- src/features/SongVisualization/SongVisualizer.tsx
+++ src/features/SongVisualization/SongVisualizer.tsx
@@ -37,67 +37,94 @@ function CanvasRenderer({
   const [mousePressed, setMousePressed] = useState(false) // TODO: mouse state shouldn't need to be ui state.
   const { width, height, measureRef } = useSize()
   const player = Player.player()
-
-  useEffect(() => {
-    // TODO If change directions, call getY
-    if (mousePressed) {
-      const wasplaying = player.isPlaying()
-      let dragY = 0
-      const handleDown = (e: MouseEvent) => {
-        if (wasplaying) {
-          player.pause()
-        }
-        // TODO: doubleclick pause / play
-        dragY = e.clientY
-        stopAccel()
-      }
-      const handleDownTouch = (e: TouchEvent) => {
-        if (wasplaying) {
-          player.pause()
-        }
-        dragY = e.targetTouches[0].clientY
-        stopAccel()
-      }
-
-      const handleUp = () => {
-        decay()
-        if (wasplaying) {
-          player.play()
-        }
-        setMousePressed(false)
-      }
-
-      const handler = (e: MouseEvent) => {
-        seekPlayer((e.clientY - dragY) * 4)
-        if (Math.abs(e.clientY - dragY) > 2) {
-          velocity(e.clientY, dragY)
-        } else velocity(0, 0)
-        dragY = e.clientY
-      }
-      const handleTouch = (e: TouchEvent) => {
-        seekPlayer((e.targetTouches[0].clientY - dragY) * 4)
-        if (Math.abs(e.targetTouches[0].clientY - dragY) > 2) {
-          velocity(e.targetTouches[0].clientY, dragY)
-        } else velocity(0, 0)
-        dragY = e.targetTouches[0].clientY
-      }
-
-      window.addEventListener('mousedown', handleDown)
-      window.addEventListener('mousemove', handler)
-      window.addEventListener('mouseup', handleUp)
-      window.addEventListener('touchstart', handleDownTouch)
-      window.addEventListener('touchmove', handleTouch)
-      window.addEventListener('touchend', handleUp)
-      return () => {
-        window.removeEventListener('mousemove', handler)
-        window.removeEventListener('mouseup', handleUp)
-        window.removeEventListener('mousedown', handleDown)
-        window.removeEventListener('touchstart', handleDownTouch)
-        window.removeEventListener('touchmove', handleTouch)
-        window.removeEventListener('touchend', handleUp)
-      }
+  // let dragY = 0
+  const [dragY, setDragY] = useState(0)
+  const [clientY, setClientY] = useState(0)
+  const handleDown = () => {
+    if (player.isPlaying()) {
+      player.pause()
     }
-  }, [mousePressed, player])
+    setDragY(0)
+    // TODO: doubleclick pause / play
+    setDragY(clientY)
+    stopAccel()
+  }
+  const handleUp = () => {
+    decay()
+    if (player.isPlaying()) {
+      player.play()
+    }
+    setMousePressed(false)
+  }
+
+  const handleMove = (e: MouseEvent | TouchEvent) => {
+    setClientY(e.clientY ? e.clientY : e.targetTouches[0].clientY)
+    seekPlayer((clientY - dragY) * 4)
+    if (Math.abs(clientY - dragY) > 2) {
+      velocity(clientY, dragY)
+    } else velocity(0, 0)
+    setDragY(clientY)
+  }
+  // useEffect(() => {
+  //   // TODO If change directions, call getY
+  //   // if (mousePressed) {
+  //   //   const wasplaying = player.isPlaying()
+  //   //   let dragY = 0
+  //   //   const handleDown = (e: MouseEvent) => {
+  //   //     if (wasplaying) {
+  //   //       player.pause()
+  //   //     }
+  //   //     // TODO: doubleclick pause / play
+  //   //     dragY = e.clientY
+  //   //     stopAccel()
+  //   //   }
+  //   //   const handleDownTouch = (e: TouchEvent) => {
+  //   //     if (wasplaying) {
+  //   //       player.pause()
+  //   //     }
+  //   //     dragY = e.targetTouches[0].clientY
+  //   //     stopAccel()
+  //   //   }
+
+  //   //   const handleUp = () => {
+  //   //     decay()
+  //   //     if (wasplaying) {
+  //   //       player.play()
+  //   //     }
+  //   //     setMousePressed(false)
+  //   //   }
+
+  //   //   const handler = (e: MouseEvent) => {
+  //   //     seekPlayer((e.clientY - dragY) * 4)
+  //   //     if (Math.abs(e.clientY - dragY) > 2) {
+  //   //       velocity(e.clientY, dragY)
+  //   //     } else velocity(0, 0)
+  //   //     dragY = e.clientY
+  //   //   }
+  //   //   const handleTouch = (e: TouchEvent) => {
+  //   //     seekPlayer((e.targetTouches[0].clientY - dragY) * 4)
+  //   //     if (Math.abs(e.targetTouches[0].clientY - dragY) > 2) {
+  //   //       velocity(e.targetTouches[0].clientY, dragY)
+  //   //     } else velocity(0, 0)
+  //   //     dragY = e.targetTouches[0].clientY
+  //   //   }
+
+  //     // window.addEventListener('mousedown', handleDown)
+  //     // window.addEventListener('mousemove', handler)
+  //     // window.addEventListener('mouseup', handleUp)
+  //     // window.addEventListener('touchstart', handleDownTouch)
+  //     // window.addEventListener('touchmove', handleTouch)
+  //     // window.addEventListener('touchend', handleUp)
+  //     return () => {
+  //       // window.removeEventListener('mousemove', handler)
+  //       // window.removeEventListener('mouseup', handleUp)
+  //       // window.removeEventListener('mousedown', handleDown)
+  //       // window.removeEventListener('touchstart', handleDownTouch)
+  //       // window.removeEventListener('touchmove', handleTouch)
+  //       // window.removeEventListener('touchend', handleUp)
+  //     }
+  //   }
+  // }, [mousePressed, player])
 
   const setupCanvas = useCallback(
     async (canvasEl: HTMLCanvasElement) => {
@@ -148,9 +175,27 @@ function CanvasRenderer({
       ref={measureRef}
       onMouseDown={(e) => {
         setMousePressed(true)
+        handleDown()
       }}
       onTouchStart={(e) => {
         setMousePressed(true)
+        handleDown()
+      }}
+      onTouchMove={(e) => {
+        if (mousePressed) {
+          handleMove(e.nativeEvent)
+        }
+      }}
+      onMouseMove={(e) => {
+        if (mousePressed) {
+          handleMove(e.nativeEvent)
+        }
+      }}
+      onTouchCancel={(e) => {
+        handleUp()
+      }}
+      onMouseUp={(e) => {
+        handleUp()
       }}
     >
       <canvas ref={setupCanvas} width={width} height={height} />
