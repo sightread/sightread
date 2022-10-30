bd70dd14e6cb62526de3d86765a75a9bc06cb706
Jake Fried <samouri@users.noreply.github.com>
2022-10-30 18:30:06 -0400

    iOS AudioContext Fix (#53)

--- src/features/synth/utils.ts
+++ src/features/synth/utils.ts
@@ -1,3 +1,4 @@
+import { isBrowser } from '@/utils'
 import { getOctave } from '../theory'
 
 // convert a MIDI.js javascript soundfont file to json
@@ -40,6 +41,38 @@ function getAudioContext() {
 function getKeyForSoundfont(note: number) {
   const soundFontIndex = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
   return soundFontIndex[note % 12] + getOctave(note)
+}
+
+if (isBrowser()) {
+  // iOS 6-8 for touchstart, iOS 9+ for touchend
+  document.addEventListener('touchstart', iOSAudioContextFix)
+  document.addEventListener('touchend', iOSAudioContextFix)
+}
+
+// iOS AC won't allow audio to play on a page unless it was created from a touch event.
+function iOSAudioContextFix() {
+  const audioContext = getAudioContext()
+  if (!audioContext) {
+    return
+  }
+
+  // Creates an empty buffer and plays it.
+  var buffer = audioContext.createBuffer(1, 1, 22050)
+  var source = audioContext.createBufferSource()
+  source.buffer = buffer
+  source.connect(audioContext.destination)
+
+  if (source.start) {
+    source.start(0)
+  } else if ((source as any).play) {
+    ;(source as any).play(0)
+  } else if ((source as any).noteOn) {
+    ;(source as any).noteOn(0)
+  }
+
+  // Remove events
+  document.removeEventListener('touchstart', iOSAudioContextFix)
+  document.removeEventListener('touchend', iOSAudioContextFix)
 }
 
 export { getAudioContext, parseMidiJsSoundfont, getKeyForSoundfont }
