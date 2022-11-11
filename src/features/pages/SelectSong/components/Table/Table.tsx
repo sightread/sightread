e639529b99dc66ad93322d24a36069977055972b
Jake <4656974+samouri@users.noreply.github.com>
2022-11-11 14:41:30 -0500

    SelectSongTable: only show scrollbar for tbody instead of also for thead.

--- src/features/pages/SelectSong/components/Table/Table.tsx
+++ src/features/pages/SelectSong/components/Table/Table.tsx
@@ -35,56 +35,52 @@ export default function Table<T extends Row>({
   const filtered = !search ? rows : rows.filter((row) => filter.some((f) => isSearchMatch(row[f])))
   const sortField = columns[Math.abs(sortCol) - 1].id
   const sorted = sortBy<T>((row) => row[sortField] ?? 0, sortCol < 0, filtered)
+  const gridTemplateColumns = `repeat(${columns.length}, 1fr)`
 
   return (
-    <div style={{ position: 'relative', flexGrow: 1 }}>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: `repeat(${columns.length}, 1fr)`,
-          position: 'absolute',
-          width: '100%',
-          maxHeight: '100%',
-          backgroundColor: '#FFF',
-          boxShadow: `0px 0px 5px rgba(0, 0, 0, 0.2)`,
-          borderRadius: 5,
-          overflowY: 'scroll',
-        }}
-      >
+    <>
+      <div className="grid" style={{ gridTemplateColumns }}>
         <TableHead
           columns={columns}
           sortCol={sortCol}
           onSelectCol={handleSelectCol}
           rowHeight={rowHeight}
         />
-        {sorted.length === 0 && <h2 className="text-2xl p-5">No results</h2>}
-        {sorted.map((row: T, i) => {
-          return (
-            <div
-              className="cursor-pointer contents group"
-              onClick={(e) => {
-                e.stopPropagation()
-                onSelectRow(getId(row))
-              }}
-              key={`row-${getId(row)}`}
-            >
-              {columns.map((col, j) => {
-                let cellValue = !!col.format ? col.format(row[col.id]) : row[col.id]
-                const paddingLeft = j === 0 ? 20 : 0
-                return (
-                  <span
-                    className="relative px-3 text-sm flex items-center flex-shrink-0 group-hover:bg-purple-hover group-even:bg-gray-100"
-                    key={`row-${i}-col-${j}`}
-                    style={{ paddingLeft, height: rowHeight }}
-                  >
-                    {cellValue}
-                  </span>
-                )
-              })}
-            </div>
-          )
-        })}
       </div>
-    </div>
+      <div className="flex flex-grow relative">
+        <div
+          className="grid absolute w-full h-full bg-white overflow-y-scroll rounded-md shadow-md"
+          style={{ gridTemplateColumns }}
+        >
+          {sorted.length === 0 && <h2 className="text-2xl p-5">No results</h2>}
+          {sorted.map((row: T, i) => {
+            return (
+              <div
+                className="cursor-pointer contents group"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  onSelectRow(getId(row))
+                }}
+                key={`row-${getId(row)}`}
+              >
+                {columns.map((col, j) => {
+                  let cellValue = !!col.format ? col.format(row[col.id]) : row[col.id]
+                  const paddingLeft = j === 0 ? 20 : 0
+                  return (
+                    <span
+                      className="relative px-3 text-sm flex items-center flex-shrink-0 group-hover:bg-purple-hover group-even:bg-gray-100"
+                      key={`row-${i}-col-${j}`}
+                      style={{ paddingLeft, height: rowHeight }}
+                    >
+                      {cellValue}
+                    </span>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
   )
 }
