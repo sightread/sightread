c966b5b13a06591b0784b360f46f4f6cb794049e
Jake F - @samouri <samouri@users.noreply.github.com>
2024-01-14 21:47:21 -0500

    Improve StartRecording Icon

--- src/icons/StartRecord.tsx
+++ src/icons/StartRecord.tsx
@@ -1,10 +1,17 @@
 import { IconProps } from '@/icons'
 
-// TODO: use a real icon set!
 export function StartRecord(props: IconProps) {
   return (
-    <svg width={props.size} height={props.size} viewBox="0 0 27 27" {...props}>
-      <circle cx="13.5" cy="13.5" r="12.5" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.size}
+      height={props.size}
+      {...props}
+      fill="currentColor"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+      <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
     </svg>
   )
 }
