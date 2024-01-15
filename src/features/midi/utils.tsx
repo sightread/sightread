0787d27f40968d769b8a76fd36dfe63973e0d7ae
Jake F - @samouri <samouri@users.noreply.github.com>
2024-01-14 20:37:53 -0500

    Run prettier

--- src/features/midi/utils.tsx
+++ src/features/midi/utils.tsx
@@ -1,62 +1,58 @@
-import { Midi } from "@tonejs/midi"
-import fs from 'fs'
+import { Midi } from '@tonejs/midi'
 import { Song, Track, Tracks, Bpm, SongNote } from '@/types'
 
 export function convertBase64MidiToSong(base64Midi: string) {
-    const binaryMidi = Buffer.from(decodeURIComponent(base64Midi), 'base64')
-    console.log(binaryMidi)
-    const midi = new Midi(binaryMidi)
-    console.log(midi)
-    let tracks: Tracks = {}
-    const bpms: Array<Bpm> = []
-    const notes: Array<SongNote> = []
-    
-    let index: number = 0
-    for (const track of midi.tracks) {
-        const newTrack: Track = {
-            instrument: track.instrument.name.replace(/ /g,"_"),
-            name: "Track" + index
-        }
-        tracks[index] = {...newTrack}
-        for (const note of track.notes) {
-            const newNote: SongNote = {
-                type: "note",
-                midiNote: note.midi,
-                duration: note.duration,
-                track: index,
-                velocity: note.velocity,
-                time: note.time,
-                measure: note.bars // ??
-            }
-            notes.push(newNote)
-        }
+  const binaryMidi = Buffer.from(decodeURIComponent(base64Midi), 'base64')
+  const midi = new Midi(binaryMidi)
+  let tracks: Tracks = {}
+  const bpms: Array<Bpm> = []
+  const notes: Array<SongNote> = []
 
-        index++
+  let index: number = 0
+  for (const track of midi.tracks) {
+    const newTrack: Track = {
+      instrument: track.instrument.name.replace(/ /g, '_'),
+      name: 'Track' + index,
+    }
+    tracks[index] = { ...newTrack }
+    for (const note of track.notes) {
+      const newNote: SongNote = {
+        type: 'note',
+        midiNote: note.midi,
+        duration: note.duration,
+        track: index,
+        velocity: note.velocity,
+        time: note.time,
+        measure: note.bars, // ??
+      }
+      notes.push(newNote)
     }
 
-    for (const tempos of midi.header.tempos) {
-        const newBpm: Bpm = {
-            time: tempos.time? tempos.time : 0,
-            bpm: tempos.bpm
-        }
-        bpms.push(newBpm)
+    index++
+  }
+
+  for (const tempos of midi.header.tempos) {
+    const newBpm: Bpm = {
+      time: tempos.time ? tempos.time : 0,
+      bpm: tempos.bpm,
     }
-    
-    // Not enough info to fill everything
-    const song: Song = {
-        tracks: tracks,
-        duration: midi.duration,
-        measures: [],
-        notes: notes,
-        bpms: bpms,
-        timeSignature: undefined,
-        keySignature: 'C',
-        items: notes,
-        backing: undefined
-    };
+    bpms.push(newBpm)
+  }
 
-    console.log(song)
+  // Not enough info to fill everything
+  const song: Song = {
+    tracks: tracks,
+    duration: midi.duration,
+    measures: [],
+    notes: notes,
+    bpms: bpms,
+    timeSignature: undefined,
+    keySignature: 'C',
+    items: notes,
+    backing: undefined,
+  }
 
-    return song
+  console.log(song)
+
+  return song
 }
-
--- src/icons/StopRecord.tsx
+++ src/icons/StopRecord.tsx
@@ -3,7 +3,7 @@ import { IconProps } from '@/icons'
 export function StopRecord(props: IconProps) {
   return (
     <svg width={props.size} height={props.size} viewBox="0 0 27 27" {...props}>
-      <rect x="3" y="3" width="21" height="21" className='fill-red-500 hover:fill-purple-hover'/>
+      <rect x="3" y="3" width="21" height="21" className="fill-red-500 hover:fill-purple-hover" />
     </svg>
   )
 }
