7e1e78eb95b7896f6ec74501969a897531a27c90
Liki <147169254+LiiiKiii@users.noreply.github.com>
2024-03-20 11:38:28 +0000

    Update ambient-types.d.ts

--- src/ambient-types.d.ts
+++ src/ambient-types.d.ts
@@ -1,6 +1,18 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 
+// Define possible events and actions as type aliases
+type GtagEvent = "page_view" | "login" | "sign_up" | "search";
+type GtagAction = "click" | "submit" | "load" | "engage";
+
+interface GtagEventObject {
+  // Add specific properties according to your actual needs
+  event_category?: string;
+  event_label?: string;
+  value?: number;
+  non_interaction?: boolean;
+}
+
 interface Window {
-  gtag: (event: string, action: string, object: any) => void
+  gtag: (event: GtagEvent, action: GtagAction, object: GtagEventObject) => void;
 }
