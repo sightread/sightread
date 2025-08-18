74e6c697349e3d796c6dadd1459a7d0daa4e314f
Shayan Toqraee <Shayan.To@Hotmail.com>
2025-08-18 19:20:18 +0330

    components/Toggle: Add a div around contents.
    This commit makes next ones more clear.

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -26,26 +26,28 @@ export default function Toggle(props: ToggleProps) {
   }, [isControlled, checked, onChange])
 
   return (
-    <label className={clsx(props.className, 'relative')} style={{ width, height }}>
-      <input
-        type="checkbox"
-        className="h-0 w-0 opacity-0"
-        onClick={toggleCheckedAndNotify}
-        style={{ margin: 0 }}
-      />
-      <span
-        className={clsx(
-          'absolute top-0 right-0 bottom-0 left-0 cursor-pointer rounded-2xl bg-gray-300 transition',
-          checked && 'bg-purple-primary!',
-        )}
-      />
-      <span
-        className={clsx(
-          'absolute left-0 mx-1 h-4 w-4 rounded-[50%] bg-white transition-all',
-          'top-1/2 -translate-y-1/2',
-          checked && 'left-[calc(100%-25px)]!',
-        )}
-      />
+    <label className={clsx(props.className)}>
+      <div className="relative" style={{ width, height }}>
+        <input
+          type="checkbox"
+          className="h-0 w-0 opacity-0"
+          onClick={toggleCheckedAndNotify}
+          style={{ margin: 0 }}
+        />
+        <span
+          className={clsx(
+            'absolute top-0 right-0 bottom-0 left-0 cursor-pointer rounded-2xl bg-gray-300 transition',
+            checked && 'bg-purple-primary!',
+          )}
+        />
+        <span
+          className={clsx(
+            'absolute left-0 mx-1 h-4 w-4 rounded-[50%] bg-white transition-all',
+            'top-1/2 -translate-y-1/2',
+            checked && 'left-[calc(100%-25px)]!',
+          )}
+        />
+      </div>
     </label>
   )
 }
