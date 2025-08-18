7383aeb54305e55d93800f6a44d13087f903b5f0
Shayan Toqraee <Shayan.To@Hotmail.com>
2025-08-18 19:40:57 +0330

    usePersistedState: Add change listener.

--- src/features/persist/hooks.ts
+++ src/features/persist/hooks.ts
@@ -1,22 +1,32 @@
 'use client'
-import { useState, useCallback, useEffect } from 'react'
+
 import { isBrowser } from '@/utils'
+import { useCallback, useEffect, useState } from 'react'
 import Storage from './storage'
 
-export function usePersistedState<T>(key: string, init: T): [T, (state: T) => void] {
+export function usePersistedState<T>(
+  key: string,
+  init: T,
+  onChange?: (state: T) => void,
+): [T, (state: T) => void] {
   const [state, setState] = useState<T>(init)
   const setPersistedState = useCallback(
     (s: T) => {
       setState(s)
       Storage.set(key, s)
+      onChange?.(s)
     },
-    [key],
+    [key, onChange],
   )
 
   // Since the initial HTML will be set from an SSR and React will only attempt to Hydrate,
   // we need to ensure any state dependent on storage renders once loaded.
-  // eslint-disable-next-line react-hooks/exhaustive-deps -- This useEffect needs to be run only once (or if ever 'key' changes)
-  useEffect(() => setState(Storage.get(key) ?? init), [key])
+  useEffect(() => {
+    const s = Storage.get<T | null>(key) ?? init
+    setState(s)
+    onChange?.(s)
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- This useEffect needs to be run only once (or if ever 'key' changes)
+  }, [key])
 
   if (!isBrowser()) {
     return [init, () => {}]
