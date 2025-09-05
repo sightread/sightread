b7b9f705d8bfad88cd32ca5984b0c870eaafd11a
emmaoberstein <11531537+emmaoberstein@users.noreply.github.com>
2025-09-05 16:42:25 -0400

    remove unused code

--- src/app/songs/page.tsx
+++ src/app/songs/page.tsx
@@ -1,19 +1,4 @@
-import { SongMetadata } from '@/types'
-import { getKey } from '@/utils'
 import ClientPage from './index'
-
-export type MidishareManifestSong = {
-  title: string
-  artist?: string
-  uploader: string
-  uploadedAt: Date // TODO: make a date type?
-  youtubeId?: string
-  originalSourceUrl?: string
-  originalSourceType: 'musescore' | 'flat.io' | 'other'
-  originalArranger: string
-  duration: string
-  midiUrl: string
-}
 
 export default async function SelectSong() {
   return <ClientPage />
